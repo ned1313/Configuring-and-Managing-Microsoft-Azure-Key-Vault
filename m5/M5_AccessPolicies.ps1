d96e61c8de229f51721f8a42ce83bac1cdf2ecd4
Ned Bellavance <ned.bellavance@gmail.com>
2019-09-17 09:44:41 -0400

    Update M5 with two files

--- m5/M5_AccessPolicies.ps1
+++ m5/M5_AccessPolicies.ps1
@@ -25,70 +25,7 @@ $keyVaultParameters = @{
 $keyVault = New-AzKeyVault @keyVaultParameters
 
 #If you already have a Key Vault
-$keyVault = Get-AzKeyVault -VaultName "VAULT_NAME" -ResourceGroupName "RESOURCE_GROUP_NAME"
-
-#Create a storage account for logs and metrics
-$storageAccountParameters = @{
-    ResourceGroupName = $keyVault.ResourceGroupName
-    Name = "$($prefix)logs$id"
-    Type = "Standard_LRS"
-    Location = $location
-}
-
-$sa = New-AzStorageAccount @storageAccountParameters
-
-#Create a Log Analytics Workspace
-$logAnalyticsParameters = @{
-    ResourceGroupName = $keyVault.ResourceGroupName
-    Name = "$prefix-keyvaultstats-$id"
-    Location = $location
-    Sku = "Standard"
-}
-
-$la = New-AzOperationalInsightsWorkspace @logAnalyticsParameters
-
-# Add solutions
-$solutionParameters = @{
-    ResourceGroupName = $la.ResourceGroupName
-    WorkspaceName = $la.Name
-    IntelligencePackName = "KeyVaultAnalytics"
-    Enabled = $true
-}
-
-# Add solutions
-Set-AzOperationalInsightsIntelligencePack @solutionParameters
-
-#Update Diagnostic Settings for Key Vault
-$diagnosticSettings = @{
-    ResourceId = $keyVault.ResourceId
-    WorkspaceId = $la.ResourceId
-    MetricCategory = "AllMetrics"
-    Category = "AuditEvent"
-    Enabled = $true
-    StorageAccountId = $sa.Id
-    RetentionEnabled = $true
-    RetentionInDays = 180
-}
-
-Set-AzDiagnosticSetting @diagnosticSettings
-
-#Generate some events
-$Secret = ConvertTo-SecureString -String 'ContosoSecrets' -AsPlainText -Force
-ForEach($n in 1..10){Add-AzKeyVaultKey -Name "key$n" -VaultName $keyVault.VaultName -Destination Software}
-ForEach($n in 1..10){Get-AzKeyVaultKey -Name "key$n" -VaultName $keyVault.VaultName}
-ForEach($n in 1..10){Set-AzKeyVaultSecret -VaultName $keyVault.VaultName -Name "secret$n" -SecretValue $Secret}
-ForEach($n in 1..10){Get-AzKeyVaultSecret -VaultName $keyVault.VaultName -Name "secret$n"}
-ForEach($n in 1..10){Remove-AzKeyVaultSecret -VaultName $keyVault.VaultName -Name "secret$n" -Force}
-
-#Create an access policy for certificates
-$certPolicyParameters = @{
-    VaultName = $keyVault.VaultName
-    ResourceGroupName = $keyVault.ResourceGroupName
-    PermissionsToCertificates = @("get","list","update","create","import","delete","recover","backup","restore")
-    UserPrincipalName = "BoWeber@contoso-ned.xyz"
-}
-
-Set-AzKeyVaultAccessPolicy @certPolicyParameters
+$keyVault = Get-AzKeyVault -VaultName "cmk-key-vault-7373" -ResourceGroupName "cmk-key-vault-7373"
 
 #Create an access policy for keys and secrets using policy
 $backupPolicyParameters = @{
